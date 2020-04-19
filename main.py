a36b0e69aba642b51443df289c7ad0b8f5f60ae6
itsMeNicoG <nicolasgongora28@gmail.com>
2020-04-19 14:58:54 -0500

    Add basic calculator

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+Practice code, basic calculator
+@Author: nGongora
+"""
+
+
+def suma(a, b):
+    return a + b
+
+
+def resta(a, b):
+    return a + b
+
+
+def multiplicacion(a, b):
+    return a + b
+
+
+def division(a, b):
+    return a + b
+
+
+resultado = suma(1, 2)
+print(str(resultado))
+
+resultado = resta(1, 2)
+print(str(resultado))
+
+resultado = multiplicacion(1, 2)
+print(str(resultado))
+
+resultado = division(1, 2)
+print(str(resultado))
