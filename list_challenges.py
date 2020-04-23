78298bc5f84308a67171ce8070ceba9fc6f82055
itsMeNicoG <nicolasgongora28@gmail.com>
2020-04-23 00:08:52 -0500

    Update list challenges

--- list_challenges.py
+++ list_challenges.py
@@ -18,3 +18,33 @@ def larger_list(lst1, lst2):
 
 
 print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+
+# check if an item appears more than n times in a list
+def more_than_n(lst, item, n):
+    if lst.count(item) > n:
+        return True
+    else:
+        return False
+
+
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
+
+# appends size of list to it
+def append_size(lst):
+    lst.append(len(lst))
+    return lst
+
+
+print(append_size([1, 2, 3, 2]))
+
+
+# zip two lists and sort result
+def combine_sort(lst1, lst2):
+    combined_list = list(zip(lst1, lst2))
+    return sorted(combined_list)
+
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
